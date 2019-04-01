4b095a6bf054fdaaf4d8523339038296535b5380
Bean <bean.yen@barco.com>
2019-04-01 19:25:13 +0800

    Draw a line in paintEvent

---
+++ painter_1/painter_1/mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPainter>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    painter.drawLine(QPointF(0, 0), QPointF(100, 100));
+}
