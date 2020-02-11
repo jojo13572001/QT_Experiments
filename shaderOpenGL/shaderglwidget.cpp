36b5c22ec574c713a701d3945cfe8ad34da4bd13
Bean <jojo13572001@gmail.com>
2020-02-12 04:01:16 +0800

    add vertex and fragment shader

--- shaderOpenGL/shaderglwidget.cpp
+++ shaderOpenGL/shaderglwidget.cpp
@@ -7,7 +7,31 @@ shaderGLWidget::shaderGLWidget(QOpenGLWidget *parent) : QOpenGLWidget(parent)
 
 void shaderGLWidget::initializeGL()
 {
-   createGPUProgram("trianglevertexshader.vert", "trianglefragmentshader.frag");
+   GLuint programId = createGPUProgram("trianglevertexshader.vert", "trianglefragmentshader.frag");
+   mOpenGLCore.reset(QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_2_Core>());
+
+   //Get Uniform ID & Attribute ID
+   mMLocationMat = mOpenGLCore->glGetUniformLocation(programId, "M");
+   mVLocationMat = mOpenGLCore->glGetUniformLocation(programId, "V");
+   mPLocationMat = mOpenGLCore->glGetUniformLocation(programId, "P");
+   mPosVector = mOpenGLCore->glGetAttribLocation(programId, "pos");
+   mColorVector = mOpenGLCore->glGetAttribLocation(programId, "color");
+
+   VertexInfo vertexInfo[3]= {{{0.0, 1.0, 0.0}, {1.0, 0.0, 0.0, 1.0}},
+                              {{-1.0, -1.0, 0.0}, {0.0, 1.0, 0.0, 1.0}},
+                              {{1.0, -1.0, 0.0}, {0.0, 0.0, 1.0, 1.0}}};
+
+   //Create VBO
+   mOpenGLCore->glGenBuffers(1, &mVBO);
+   mOpenGLCore->glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+   mOpenGLCore->glBufferData(GL_ARRAY_BUFFER, sizeof(VertexInfo) * 3, vertexInfo, GL_STATIC_DRAW);
+   mOpenGLCore->glBindBuffer(GL_ARRAY_BUFFER, 0);
+   //create IBO
+   unsigned int index[3] = {0, 1, 2};
+   mOpenGLCore->glGenBuffers(1, &mIBO);
+   mOpenGLCore->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIBO);
+   mOpenGLCore->glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+   mOpenGLCore->glBindBuffer(GL_ARRAY_BUFFER, 0);
 
    glClearColor(0.0, 0.0, 0.0, 0.0);
    glShadeModel(GL_SMOOTH);
@@ -31,6 +55,7 @@ void shaderGLWidget::paintGL()
    glLoadIdentity();
 
    glTranslatef(-2.0, 0.0, -6.0);
+   /*
    glBegin(GL_TRIANGLES);
    glColor3f(1.0, 0.0, 0.0);
    glVertex3f(0.0, 1.0, 0.0);
@@ -39,7 +64,8 @@ void shaderGLWidget::paintGL()
    glColor3f(0.0, 0.0, 1.0);
    glVertex3f(1.0, -1.0, 0.0);
    glEnd();
-
+   */
+    /*
    glTranslatef(4.0, 0.0, 0.0);
    glBegin(GL_QUADS);
    glColor3f(1.0, 1.0, 0.0);
@@ -48,6 +74,32 @@ void shaderGLWidget::paintGL()
    glVertex3f(1.0, -1.0, 0.0);
    glVertex3f(-1.0, -1.0, 0.0);
    glEnd();
+   */
+
+   if (!mShaderProgram->bind())
+       return;
+   QMatrix4x4 nMnormalMat, nVnormalMat, projectionMatrix;
+
+   mOpenGLCore->glUniformMatrix4fv(mMLocationMat, 1, GL_FALSE, nMnormalMat.data());
+   mOpenGLCore->glUniformMatrix4fv(mVLocationMat, 1, GL_FALSE, nVnormalMat.data());
+   mOpenGLCore->glUniformMatrix4fv(mPLocationMat, 1, GL_FALSE, projectionMatrix.data());
+
+   mOpenGLCore->glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+
+   // DrawArrays paint
+   mOpenGLCore->glEnableVertexAttribArray(mPosVector);
+   mOpenGLCore->glVertexAttribPointer(mPosVector, 3, GL_FLOAT, GL_FALSE, sizeof(VertexInfo), (void*)0);
+   mOpenGLCore->glEnableVertexAttribArray(mColorVector);
+   mOpenGLCore->glVertexAttribPointer(mColorVector, 4, GL_FLOAT, GL_FALSE, sizeof(VertexInfo), (void*)(sizeof(float) * 3));
+   //OpenGLCore->glDrawArrays(GL_TRIANGLES, 0, 3);
+   mOpenGLCore->glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+   // DrawElement paint
+   mOpenGLCore->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIBO);
+   mOpenGLCore->glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+   mOpenGLCore->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+   mShaderProgram->release();
 }
 
 GLuint shaderGLWidget::createGPUProgram(const QString& vertexShaderFile, const QString& fragmentShaderFile)
--- shaderOpenGL/shaderglwidget.h
+++ shaderOpenGL/shaderglwidget.h
@@ -5,9 +5,18 @@
 #include <QOpenGLWidget>
 #include <QOpenGLShader>
 #include <QOpenGLShaderProgram>
+//#include <QOpenGLFunctions>
+#include <QOpenGLFunctions_3_2_Core>
 #include <GL/glu.h>
 
 using namespace std;
+
+struct VertexInfo
+{
+    float pos[3];
+    float color[4];
+};
+
 class shaderGLWidget : public QOpenGLWidget
 {
     Q_OBJECT
@@ -20,9 +29,23 @@ private:
     GLuint createGPUProgram(const QString& nVertexShaderFile, const QString& nFragmentShaderFile);
 
 private:
-    unique_ptr<QOpenGLShader> mVertexShader;
-    unique_ptr<QOpenGLShader> mFragmentShader;
-    unique_ptr<QOpenGLShaderProgram> mShaderProgram;
+    unique_ptr<QOpenGLShader> mVertexShader{nullptr};
+    unique_ptr<QOpenGLShader> mFragmentShader{nullptr};
+    unique_ptr<QOpenGLShaderProgram> mShaderProgram{nullptr};
+
+    unique_ptr<QOpenGLFunctions_3_2_Core> mOpenGLCore{nullptr};
+
+    // MVP Location
+    GLint mMLocationMat;
+    GLint mVLocationMat;
+    GLint mPLocationMat;
+    GLint mPosVector;
+    GLint mColorVector;
+
+    // VBO
+    GLuint mVBO;
+    // IBO
+    GLuint mIBO;
 
 protected:
     void initializeGL();
